b77db9a5c719fde72f360be6288bf884d807319d
Gavin Cooper <phd@gavincooper.net>
2021-05-27 14:01:12 +1000

    Move config to sep files, blocks and break added

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
-_version = '0.0.2'
+__version__ = '0.0.3'
+sound_set = [
+    'Central_100ms.wav', 'Central_50ms.wav', 'left_22.5deg_100ms.wav',
+    'left_22.5deg_50ms.wav', 'left_45deg_100ms.wav', 'left_45deg_50ms.wav',
+    'left_67deg_100ms.wav', 'left_67deg_50ms.wav', 'left_90deg_100ms.wav',
+    'left_90deg_50ms.wav', 'right_22.5deg_100ms.wav', 'right_22.5deg_50ms.wav',
+    'right_45deg_100ms.wav', 'right_45deg_50ms.wav', 'right_67deg_100ms.wav',
+    'right_67deg_50ms.wav', 'right_90deg_100ms.wav', 'right_90deg_50ms.wav'
+]
+sound_files = ['stimuli/' + s for s in sound_set]
+classes = ['std' if '50ms' in s else 'dev' for s in sound_set]
+counts = {
+    'hvc': [145 if c == 'std' else 15 for c in classes],
+    'lvc': [263 if c == 'std' else 25 for c in classes]
+}
+# Fix low variance condition counts (remove 67 and 90 degree sounds entirely)
+for idx, snd in enumerate(sound_set):
+    if '67deg' in snd or '90deg' in snd:
+        counts['lvc'][idx] = 0
--- test.py
+++ test.py
@@ -17,123 +17,100 @@ from __future__ import absolute_import, division
 
 from psychopy import sound, gui, visual, core, data, logging, clock, parallel
 from psychopy.constants import (NOT_STARTED, STARTED, FINISHED)
+from psychopy.hardware import keyboard
+from random import shuffle, choice
 import os  # handy system and path functions
 
-from psychopy.hardware import keyboard
+# Import task specific details (sounds, counts etc)
+from config import sound_set, sound_files, classes, counts, __version__
 
-import config
-
-sound_set = [
-    'Central_100ms.wav', 'Central_50ms.wav', 'left_22.5deg_100ms.wav',
-    'left_22.5deg_50ms.wav', 'left_45deg_100ms.wav', 'left_45deg_50ms.wav',
-    'left_67deg_100ms.wav', 'left_67deg_50ms.wav', 'left_90deg_100ms.wav',
-    'left_90deg_50ms.wav', 'right_22.5deg_100ms.wav', 'right_22.5deg_50ms.wav',
-    'right_45deg_100ms.wav', 'right_45deg_50ms.wav', 'right_67deg_100ms.wav',
-    'right_67deg_50ms.wav', 'right_90deg_100ms.wav', 'right_90deg_50ms.wav'
-]
-sound_files = ['stimuli/' + s for s in sound_set]
+# Important globals
 sounds = [sound.Sound(sound_files[s_idx], name=s)
           for s_idx, s in enumerate(sound_set)]
-classes = ['std' if '50ms' in s else 'dev' for s in sound_set]
-hvc_counts = [145 if c == 'std' else 15 for c in classes]
-lvc_counts = [263 if c == 'std' else 25 for c in classes]
-for idx, snd in enumerate(sound_set):
-    if '67deg' in snd or '90deg' in snd:
-        lvc_counts[idx] = 0
-hvc_list = []
-for idx, snd in enumerate(sounds):
-    hvc_list += hvc_counts[idx] * [{
-        'stim_type': classes[idx],
-        'sound': snd,
-        'name': sound_set[idx].split('.')[0],
-        'code': idx
-    }]
 
 
-# Ensure that relative paths start from the same directory as this script
-_thisDir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(_thisDir)
+def select_stimuli(types, count_tracker):
+    """Select a stimulus match the type from the provided array
+    :returns: a dict for the stimulus chosen
+    """
+    type_index = choice(types)
+    count_tracker[type_index] -= 1
+    if count_tracker[type_index] == 0:
+        del(types[types.index(type_index)])
+    return type_index
 
-# Store info about the experiment session
-psychopyVersion = '2021.1.4'  # GC: Check this
-expName = 'Mattsen_Study3'
-expInfo = {'participant': '', 'session': '001'}
-dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName)
-if not dlg.OK:
-    core.quit()  # user pressed cancel
-expInfo['date'] = data.getDateStr()  # add a simple timestamp
-expInfo['expName'] = expName
-expInfo['psychopyVersion'] = psychopyVersion
 
-# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
-filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
+def get_snd_dict(snd_index):
+    """Return a dictionary representing the sound
+    """
+    return {
+        'stim_type': classes[snd_index],
+        'sound': sounds[snd_index],
+        'name': sound_set[snd_index].split('.')[0],
+        'code': snd_index
+    }
 
-# An ExperimentHandler isn't essential but helps with data saving
-thisExp = data.ExperimentHandler(
-    name=expName, version=config._version,
-    extraInfo=expInfo, runtimeInfo=None,
-    originPath=_thisDir,  # GC: update
-    savePickle=True, saveWideText=True,
-    dataFileName=filename)
-# save a log file for detail verbose info
-logFile = logging.LogFile(filename + '.log', level=logging.EXP)
-logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
 
-endExpNow = False  # flag for 'escape' or other condition => quit the exp
-frameTolerance = 0.001  # how close to onset before 'same' frame
+def generate_sequence(sequence_type):
+    """
+    For a particular condition, generate a sequence of sounds that fulfills
+    the requirements of the condition.
 
-# Start Code - component code to be run after the window creation
+    :returns: A list of dictionaries, each element of the list represents a
+    trial in the final block.
 
-# Setup the Window
-# GC: Update for lab
-win = visual.Window(
-    size=[1920, 1080], fullscr=True, screen=0,
-    winType='pyglet', allowGUI=False, allowStencil=False,
-    monitor='testMonitor', color=[0, 0, 0], colorSpace='rgb',
-    blendMode='avg', useFBO=True,
-    units='height')
-# store frame rate of monitor if we can measure it
-expInfo['frameRate'] = win.getActualFrameRate()
-if expInfo['frameRate'] is not None:
-    frameDur = 1.0 / round(expInfo['frameRate'])
-else:
-    frameDur = 1.0 / 60.0  # could not measure, so guess
+    """
+    # A sequence is 1 std, followed by a random shuffled array of stds and ssd,
+    # where ssd represents three trials - std, std, dev.
+    length = sum(counts[sequence_type])  # Total length of block
+    num_devs = sum(c for i, c in enumerate(counts[sequence_type])
+                   if classes[i] == 'dev')
+    # total plain standards = total - first std - number of trials in ssd
+    num_stds = length - 1 - num_devs * 3
 
-# create a default keyboard (e.g. to check for escape)
-defaultKeyboard = keyboard.Keyboard()
+    sequence = ['std'] * num_stds + ['ssd'] * num_devs
+    shuffle(sequence)
+    tracker = counts[sequence_type].copy()
+    stds = [i for i, c in enumerate(classes) if c == 'std']
+    devs = [i for i, c in enumerate(classes) if c == 'dev']
+    stimuli_list = []
 
-# Initialize components for Routine "trial"
-trialClock = core.Clock()
-SOA = clock.StaticPeriod(win=win, screenHz=expInfo['frameRate'], name='SOA')
+    # First standard
+    idx = select_stimuli(stds, tracker)
+    stimuli_list.append(get_snd_dict(idx))
 
-# Create some handy timers
-globalClock = core.Clock()  # to track the time since experiment started
-routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine
+    # All std/sdd's in sequence
+    for s in sequence:
+        if s == 'std':
+            idx = select_stimuli(stds, tracker)
+            stimuli_list.append(get_snd_dict(idx))
+        elif s == 'ssd':
+            idx = select_stimuli(stds, tracker)
+            stimuli_list.append(get_snd_dict(idx))
+            idx = select_stimuli(stds, tracker)
+            stimuli_list.append(get_snd_dict(idx))
+            idx = select_stimuli(devs, tracker)
+            stimuli_list.append(get_snd_dict(idx))
+        else:
+            raise RuntimeError('Unexpected sequence value')
+    return stimuli_list
 
-# set up handler to look after randomisation of conditions etc
-test_block = data.TrialHandler(
-    nReps=1, method='random',
-    extraInfo=expInfo, originPath=-1,
-    trialList=hvc_list,
-    seed=None, name='test_block')
-thisExp.addLoop(test_block)  # add the loop to the experiment
-thisTrial = test_block.trialList[0]  # so we can initialise stimuli with some values
-# abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
-if thisTrial is not None:
-    for paramName in thisTrial:
-        exec('{} = thisTrial[paramName]'.format(paramName))
 
-for thisTrial in test_block:
-    currentLoop = test_block
-    # abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
-    if thisTrial is not None:
-        for paramName in thisTrial:
-            exec('{} = thisTrial[paramName]'.format(paramName))
+def rest_break():
+    """Rest for five minutes and return
+    """
+    rest = visual.TextStim(
+        win=win, name='rest', text='5 minutes rest break', font='Open Sans',
+        pos=(0, 0), height=0.1, wrapWidth=None, ori=0.0, color='white',
+        colorSpace='rgb', opacity=None, languageStyle='LTR', depth=0.0
+    )
 
     # ------Prepare to start Routine "trial"-------
     continueRoutine = True
+    routineTimer.add(300.000000)
+    # update component parameters for each repeat
     # keep track of which components have finished
-    trialComponents = [sound, SOA]
+    trialComponents = [rest]
     for thisComponent in trialComponents:
         thisComponent.tStart = None
         thisComponent.tStop = None
@@ -148,31 +125,30 @@ for thisTrial in test_block:
     frameN = -1
 
     # -------Run Routine "trial"-------
-    while continueRoutine:
+    while continueRoutine and routineTimer.getTime() > 0:
         # get current time
         t = trialClock.getTime()
         tThisFlip = win.getFutureFlipTime(clock=trialClock)
         tThisFlipGlobal = win.getFutureFlipTime(clock=None)
         frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
         # update/draw components on each frame
-        # start/stop sound
-        if sound.status == NOT_STARTED and t >= 0.0 - frameTolerance:
+
+        # *rest* updates
+        if rest.status == NOT_STARTED and tThisFlip >= 0.0 - frameTolerance:
             # keep track of start time/frame for later
-            sound.frameNStart = frameN  # exact frame index
-            sound.tStart = t  # local t and not account for scr refresh
-            sound.tStartRefresh = tThisFlipGlobal  # on global time
-            sound.play()  # start the sound (it finishes automatically)
-        # *SOA* period
-        if SOA.status == NOT_STARTED and t >= 0.0 - frameTolerance:
-            # keep track of start time/frame for later
-            SOA.frameNStart = frameN  # exact frame index
-            SOA.tStart = t  # local t and not account for scr refresh
-            SOA.tStartRefresh = tThisFlipGlobal  # on global time
-            win.timeOnFlip(SOA, 'tStartRefresh')  # time at next scr refresh
-            SOA.start(0.5)
-        elif SOA.status == STARTED:  # one frame should pass before updating params and completing
-            SOA.complete()  # finish the static period
-            SOA.tStop = SOA.tStart + 0.5  # record stop time
+            rest.frameNStart = frameN  # exact frame index
+            rest.tStart = t  # local t and not account for scr refresh
+            rest.tStartRefresh = tThisFlipGlobal  # on global time
+            win.timeOnFlip(rest, 'tStartRefresh')  # time at next scr refresh
+            rest.setAutoDraw(True)
+        if rest.status == STARTED:
+            # is it time to stop? (based on global clock, using actual start)
+            if tThisFlipGlobal > rest.tStartRefresh + 300.0 - frameTolerance:
+                # keep track of stop time/frame for later
+                rest.tStop = t  # not accounting for scr refresh
+                rest.frameNStop = frameN  # exact frame index
+                win.timeOnFlip(rest, 'tStopRefresh')  # time at next scr refresh
+                rest.setAutoDraw(False)
 
         # check for quit (typically the Esc key)
         if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
@@ -195,16 +171,171 @@ for thisTrial in test_block:
     for thisComponent in trialComponents:
         if hasattr(thisComponent, "setAutoDraw"):
             thisComponent.setAutoDraw(False)
-    sound.stop()  # ensure sound has stopped at end of routine
-    test_block.addData('sound.started', sound.tStart)
-    test_block.addData('sound.stopped', sound.tStop)
-    test_block.addData('SOA.started', SOA.tStart)
-    test_block.addData('SOA.stopped', SOA.tStop)
+    thisExp.addData('rest.started', rest.tStartRefresh)
+    thisExp.addData('rest.stopped', rest.tStopRefresh)
 
-    routineTimer.reset()
-    thisExp.nextEntry()
 
-# completed 100.0 repeats of 'test_block'
+# Setup (taken from Psychopy generated code and adjusted slightly)
+# Ensure that relative paths start from the same directory as this script
+_thisDir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(_thisDir)
+
+# Store info about the experiment session
+psychopyVersion = '2021.1.4'  # GC: Check this
+expName = 'Mattsen_Study3'
+expInfo = {'participant': '', 'group': '1'}
+dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName)
+if not dlg.OK:
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+expInfo['psychopyVersion'] = psychopyVersion
+
+# Data file name stem = absolute path + name; later add .psyexp, .csv, .log, etc
+filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expName, expInfo['date'])
+
+# An ExperimentHandler isn't essential but helps with data saving
+thisExp = data.ExperimentHandler(
+    name=expName, version=__version__, extraInfo=expInfo, runtimeInfo=None,
+    originPath=_thisDir, savePickle=True, saveWideText=True,
+    dataFileName=filename
+)
+# save a log file for detail verbose info
+logFile = logging.LogFile(filename + '.log', level=logging.EXP)
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
+
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
+frameTolerance = 0.001  # how close to onset before 'same' frame
+
+# Start Code - component code to be run after the window creation
+
+# Setup the Window
+# GC: Update for lab
+win = visual.Window(
+    size=[1920, 1080], fullscr=True, screen=0, winType='pyglet',
+    allowGUI=False, allowStencil=False, monitor='testMonitor', color=[0, 0, 0],
+    colorSpace='rgb', blendMode='avg', useFBO=True, units='height'
+)
+# store frame rate of monitor if we can measure it
+expInfo['frameRate'] = win.getActualFrameRate()
+if expInfo['frameRate'] is not None:
+    frameDur = 1.0 / round(expInfo['frameRate'])
+else:
+    frameDur = 1.0 / 60.0  # could not measure, so guess
+
+# create a default keyboard (e.g. to check for escape)
+defaultKeyboard = keyboard.Keyboard()
+
+# Initialize components for Routine "trial"
+trialClock = core.Clock()
+SOA = clock.StaticPeriod(win=win, screenHz=expInfo['frameRate'], name='SOA')
+
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine
+
+if expInfo['group'] == '1':
+    blocks = ['hvc', 'lvc', 'break', 'lvc', 'hvc']
+else:
+    blocks = ['lvc', 'hvc', 'break', 'hvc', 'lvc']
+
+for block in blocks:
+    if block == 'break':
+        rest_break()
+        continue
+    # Otherwise we are in a stimulus delivery block
+    # set up handler to look after randomisation of conditions etc
+    test_block = data.TrialHandler(
+        nReps=1, method='sequential', extraInfo=expInfo, originPath=-1,
+        trialList=generate_sequence(block), seed=None, name=block + '_block'
+    )
+    thisExp.addLoop(test_block)  # add the loop to the experiment
+    thisTrial = test_block.trialList[0]  # so we can initialise stimuli with some values
+    # abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
+    if thisTrial is not None:
+        for paramName in thisTrial:
+            exec('{} = thisTrial[paramName]'.format(paramName))
+
+    for thisTrial in test_block:
+        currentLoop = test_block
+        # abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
+        if thisTrial is not None:
+            for paramName in thisTrial:
+                exec('{} = thisTrial[paramName]'.format(paramName))
+
+        # ------Prepare to start Routine "trial"-------
+        continueRoutine = True
+        # keep track of which components have finished
+        trialComponents = [sound, SOA]
+        for thisComponent in trialComponents:
+            thisComponent.tStart = None
+            thisComponent.tStop = None
+            thisComponent.tStartRefresh = None
+            thisComponent.tStopRefresh = None
+            if hasattr(thisComponent, 'status'):
+                thisComponent.status = NOT_STARTED
+        # reset timers
+        t = 0
+        _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+        trialClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+        frameN = -1
+
+        # -------Run Routine "trial"-------
+        while continueRoutine:
+            # get current time
+            t = trialClock.getTime()
+            tThisFlip = win.getFutureFlipTime(clock=trialClock)
+            tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+            frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+            # update/draw components on each frame
+            # start/stop sound
+            if sound.status == NOT_STARTED and t >= 0.0 - frameTolerance:
+                # keep track of start time/frame for later
+                sound.frameNStart = frameN  # exact frame index
+                sound.tStart = t  # local t and not account for scr refresh
+                sound.tStartRefresh = tThisFlipGlobal  # on global time
+                sound.play()  # start the sound (it finishes automatically)
+            # *SOA* period
+            if SOA.status == NOT_STARTED and t >= 0.0 - frameTolerance:
+                # keep track of start time/frame for later
+                SOA.frameNStart = frameN  # exact frame index
+                SOA.tStart = t  # local t and not account for scr refresh
+                SOA.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(SOA, 'tStartRefresh')  # time at next scr refresh
+                SOA.start(0.5)
+            elif SOA.status == STARTED:  # one frame should pass before updating params and completing
+                SOA.complete()  # finish the static period
+                SOA.tStop = SOA.tStart + 0.5  # record stop time
+
+            # check for quit (typically the Esc key)
+            if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+                core.quit()
+
+            # check if all components have finished
+            if not continueRoutine:  # a component has requested a forced-end of Routine
+                break
+            continueRoutine = False  # will revert to True if at least one component still running
+            for thisComponent in trialComponents:
+                if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                    continueRoutine = True
+                    break  # at least one component has not yet finished
+
+            # refresh the screen
+            if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+                win.flip()
+
+        # -------Ending Routine "trial"-------
+        for thisComponent in trialComponents:
+            if hasattr(thisComponent, "setAutoDraw"):
+                thisComponent.setAutoDraw(False)
+        sound.stop()  # ensure sound has stopped at end of routine
+        test_block.addData('sound.started', sound.tStart)
+        test_block.addData('sound.stopped', sound.tStop)
+        test_block.addData('SOA.started', SOA.tStart)
+        test_block.addData('SOA.stopped', SOA.tStop)
+
+        routineTimer.reset()
+        thisExp.nextEntry()
 
 
 # Flip one final time so any remaining win.callOnFlip()
