a0bd673e1ef67e95b46463ccbe4af9c3d267d194
Gavin Cooper <phd@gavincooper.net>
2021-07-27 12:26:14 +1000

    Lower variance in LVC, NB updates for spatial work

--- config.py
+++ config.py
@@ -13,22 +13,20 @@ sound_files = ['stimuli/' + s for s in sound_set]
 classes = ['std' if '50ms' in s else 'dev' for s in sound_set]
 counts = {
     'hvc': [145 if c == 'std' else 15 for c in classes],
-    'lvc': [261 if c == 'std' else 27 for c in classes]
+    'lvc': [435 if c == 'std' else 45 for c in classes],
 }
 if debug:
     counts = {
-        'hvc': [5 if c == 'std' else 1 for c in classes],
-        'lvc': [5 if c == 'std' else 1 for c in classes]
+        'hvc': [29 if c == 'std' else 3 for c in classes],
+        'lvc': [87 if c == 'std' else 9 for c in classes]
     }
 
 # Fix low variance condition counts (remove 67 and 90 degree sounds entirely)
 for idx, snd in enumerate(sound_set):
-    if '67deg' in snd or '90deg' in snd:
+    if '67deg' in snd or '90deg' in snd or '45deg' in snd:
         counts['lvc'][idx] = 0
 
 soa_time = 0.5
-if debug:
-    soa_time = 0.2
 code_length = 0.1
 rest_time = 300
 if debug:
